3961da283c42ff304ddb6934cdeb542a6f115645
Lucas Silva <lucassilva.uern@gmail.com>
2024-11-01 21:33:43 -0300

    feat: classe principal do cucumber que roda os testes

---
+++ src/test/java/org/mobile/bdd/run/CucumberTest.java
@@ -0,0 +1,15 @@
+package org.mobile.bdd.run;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("org/mobile/bdd")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.mobile.bdd")
+public class CucumberTest {
+}
