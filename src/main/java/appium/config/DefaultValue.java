e8795420b21353dbc06589568553ee3fca163d08
Lucas Silva <lucassilva.uern@gmail.com>
2024-11-01 21:18:58 -0300

    feat: classe de constantes utilizadas na classe que contem o appium driver

---
+++ src/main/java/appium/config/DefaultValue.java
@@ -0,0 +1,22 @@
+package appium.config;
+
+public class DefaultValue {
+    // Nomes do APP
+    public static final String NAME_APK = "qafood.apk";
+
+    // Nomes device
+    public static final String NAME_DEVICE = "emulator-5554";
+
+    // Config Path
+    public static final String PATH_APP = "src/main/java/appium/apk/" + NAME_APK;
+    public static final String PATH_URL_APPIUM = "http://localhost:4724/";
+
+    // Config Plataforma
+    public static final String PLATFORM_ANDROID = "Android";
+
+    // Config Time
+    public static final int TIME_SERVER = 30;
+    public static final int TIME_DEFAULT = 5;
+
+    public static final int TIMEOUT_PRESENCE_OF_ELEMENT_LOCATED_SECONDS = 60;
+}
