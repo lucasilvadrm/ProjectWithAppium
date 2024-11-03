d2e9bd6a3e7aafa75024a646cc8316de7a823098
Lucas Silva <lucassilva.uern@gmail.com>
2024-11-02 21:30:01 -0300

    config: configuracao comentada

--- src/test/java/org/mobile/bdd/run/CucumberTest.java
+++ src/test/java/org/mobile/bdd/run/CucumberTest.java
@@ -5,11 +5,13 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.junit.platform.engine.Constants.FILTER_TAGS_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("org/mobile/bdd")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.mobile.bdd")
+//@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@important")
 public class CucumberTest {
 }
